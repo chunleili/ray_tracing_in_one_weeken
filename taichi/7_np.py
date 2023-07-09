35fa1a2dfe45cb53242c129ff43f5877fbb9f113
chunleili <li_cl@foxmail.com>
2023-07-09 18:24:16 +0800

    [new] Add Metal for python

--- taichi/7_np.py
+++ taichi/7_np.py
@@ -10,6 +10,43 @@ height = int(width / aspect_ratio)  # 图像高度
 channels = 3  # RGB图像，通道数为3
 
 
+class Material:
+    @abstractmethod
+    def scatter(self, r_in, rec, attenuation, scattered):
+        ...
+
+
+class Lambertian(Material):
+    def __init__(self, albedo:np.ndarray):
+        self.albedo = albedo
+
+    def scatter(self, r_in, rec, attenuation, scattered)->bool:
+        scatter_direction = rec.normal + random_unit_vector()
+
+        if np.allclose(scatter_direction, np.array([0., 0, 0]), atol=1e-8): 
+            scatter_direction = rec.normal
+
+        scattered = Ray(rec.p, scatter_direction)
+        attenuation = self.albedo
+        return True, scattered, attenuation
+
+
+def reflect(v:np.ndarray, n:np.ndarray)->np.ndarray:
+    return v - 2*np.dot(v, n)*n
+
+
+class Metal(Material):
+    def __init__(self, albedo:np.ndarray):
+        self.albedo = albedo
+
+    def scatter(self, r_in, rec, attenuation, scattered)->bool:
+        reflected = reflect(r_in.dir, rec.normal)
+        scattered = Ray(rec.p, reflected)
+        attenuation = self.albedo
+        return np.dot(scattered.dir, rec.normal) > 0, scattered, attenuation
+
+
+
 class Camera:
     def __init__(self):
         aspect_ratio = 16.0 / 9.0
@@ -20,7 +57,7 @@ class Camera:
         self.origin = np.array([0., 0, 0])
         self.horizontal = np.array([viewport_width, 0, 0])
         self.vertical = np.array([0., viewport_height, 0])
-        self.lower_left_corner = self.origin - self.horizontal/2 - self.vertical/2 - np.array([0, 0, focal_length])
+        self.lower_left_corner = self.origin - self.horizontal/2 - self.vertical/2 - np.array([0., 0, focal_length])
 
     def get_ray(self, u, v):
         return Ray(self.origin, self.lower_left_corner + u*self.horizontal + v*self.vertical - self.origin)
@@ -34,9 +71,10 @@ class Hittable:
 
 
 class Sphere(Hittable):
-    def __init__(self, center, radius):
+    def __init__(self, center, radius, mat_ptr:Material):
         self.center = center
         self.radius = radius
+        self.mat_ptr = mat_ptr
 
     def hit(self, r, t_min, t_max, rec)->bool:
         oc = r.orig - self.center
@@ -62,16 +100,18 @@ class Sphere(Hittable):
                 rec.p = r.at(rec.t)
                 outward_normal = (rec.p - self.center) / self.radius
                 rec.set_face_normal(r, outward_normal)
+                rec.mat_ptr = self.mat_ptr
                 res = True
         return res
 
 
 @dataclass
 class HitRecord:
-    p: np.array
-    normal: np.array
-    t: float
-    front_face: bool
+    p: np.array = np.array([0., 0, 0])
+    normal: np.array = np.array([0., 0, 0])
+    mat_ptr: Material = Lambertian(np.array([0., 0, 0]))
+    t: float = 0.0
+    front_face: bool = False
     def set_face_normal(self, r, outward_normal):
         self.front_face = r.dir.dot(outward_normal) < 0
         self.normal = outward_normal if self.front_face else -outward_normal
@@ -111,23 +151,33 @@ def random_in_hemisphere(normal):
         return in_unit_sphere
     else:
         return -in_unit_sphere
+    
+def random_unit_vector():
+    return random_in_unit_sphere() / np.linalg.norm(random_in_unit_sphere())
 
 @dataclass
 class Ray:
-    orig: np.array
-    dir: np.array
+    orig: np.array = np.array([0., 0, 0])
+    dir: np.array = np.array([0., 0, 0])
     def at(self, t):
         return self.orig + t*self.dir
 
+
 def ray_color(r, world, depth=0):
     #  If we've exceeded the ray bounce limit, no more light is gathered.
-    if (depth <= 0):
+    if (depth <= 0.):
         return np.array([0., 0, 0])
-    rec = HitRecord(np.array([0., 0, 0]), np.array([0., 0, 0]), 0.0, False)
+    rec = HitRecord()
     res = np.array([0., 0, 0])
+
     if world.hit(r, 0.001, float('inf'), rec):
-        target = rec.p + random_in_hemisphere(rec.normal)
-        res = 0.5 * ray_color(Ray(rec.p, target - rec.p), world, depth-1)
+        scattered = Ray()
+        attenuation = np.array([0., 0, 0])
+        flag, scattered, attenuation = rec.mat_ptr.scatter(r, rec, attenuation=Ray, scattered=np.array([0., 0, 0]))
+        if flag:
+            return attenuation * ray_color(scattered, world, depth-1)
+        else:
+            return np.array([0., 0, 0])
     else:
         unit_direction = r.dir / np.linalg.norm(r.dir)
         t = 0.5 * (unit_direction[1] + 1.0)
@@ -185,8 +235,19 @@ def write_ppm(image_data):
 
 def main():
     world = HittableList()
-    world.add(Sphere(np.array([0., 0, -1]), 0.5))
-    world.add(Sphere(np.array([0, -100.5, -1]), 100))
+    world.add(Sphere(np.array([0., 0, -1]), 0.5, Lambertian(np.array([0.7, 0.3, 0.3]))))
+    world.add(Sphere(np.array([0, -100.5, -1]), 100, Lambertian(np.array([0.8, 0.8, 0.0]))))
+
+    material_ground = Lambertian(np.array([0.8, 0.8, 0.0]))
+    material_center = Lambertian(np.array([0.7, 0.3, 0.3]))
+    material_left   = Metal(np.array([0.8, 0.8, 0.8]))
+    material_right  = Metal(np.array([0.8, 0.6, 0.2]))
+
+    world.add(Sphere(np.array([0., -100.5, -1]), 100, material_ground))
+    world.add(Sphere(np.array([0., 0, -1]), 0.5, material_center))
+    world.add(Sphere(np.array([-1., 0, -1]), 0.5, material_left))
+    world.add(Sphere(np.array([1., 0, -1]), 0.5, material_right))
+
 
     cam = Camera()
 
